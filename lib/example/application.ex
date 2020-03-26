8b7c79330ed2db7d336d14c739691563ccef6b2e
Philihp Busby <philihp@gmail.com>
2020-03-26 23:27:44 +0000

    add libcluster supervisor only if available

--- lib/example/application.ex
+++ lib/example/application.ex
@@ -4,11 +4,11 @@ defmodule Example.Application do
   def start(_type, _args) do
     import Supervisor.Spec
 
-    children = [
-      supervisor(Cluster.Supervisor, [topologies, [name: Example.ClusterSupervisor]]),
-      supervisor(Example.Database, []),
-      supervisor(ExampleWeb.Endpoint, []),
-    ]
+    children =
+      [
+        supervisor(Example.Database, []),
+        supervisor(ExampleWeb.Endpoint, [])
+      ] ++ libcluster()
 
     opts = [strategy: :one_for_one, name: Example.Supervisor]
     Supervisor.start_link(children, opts)
@@ -17,5 +17,13 @@ defmodule Example.Application do
   def config_change(changed, _new, removed) do
     ExampleWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp libcluster(topologies \\ Application.get_env(:libcluster, :topologies))
+
+  defp libcluster(nil), do: []
+
+  defp libcluster(topologies) do
+    [{Cluster.Supervisor, [topologies, [name: Example.ClusterSupervisor]]}]
   end
 end
